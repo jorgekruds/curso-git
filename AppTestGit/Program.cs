9f7ad26a346c3b117185f1d98762949e8d0a1fd0
Jorge L L Resende <jorgekruds@hotmail.com>
2021-11-01 10:28:40 -0300

    Projeto criado e so imprime Ola Mundo.

---
+++ AppTestGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppTestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundão!");
+        }
+    }
+}
